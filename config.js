70369bf669d53d006446f91852bb14e1e04f186e
Niall Curran <niallcurran95@gmail.com>
2016-10-27 15:08:32 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function config() {
 
 module.exports = config;
 
-config.mongoUri = 'mongodb://ds033607.mlab.com:33607/heroku_fbnj3c1m';
+config.mongoUri = 'mongodb://test:test@ds033607.mlab.com:33607/heroku_fbnj3c1m';
 
 config.SimplifyPay = Simplify.getClient({
     publicKey: 'sbpb_NWVkNzAxMDctYmI4MS00N2Y5LThkYzAtYTU5NjgzZjI3ZTA5',
