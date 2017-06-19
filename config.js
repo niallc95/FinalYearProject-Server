b8d25ab5d5076a964e7984614516dcd7a29d1a60
Niall Curran <niallcurran95@gmail.com>
2017-06-19 14:48:32 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,10 @@
 var Simplify = require('simplify-commerce');
-// constructor
-function config() {
-}
 
 module.exports = config;
 
 config.mongoUri = 'mongodb://test:test@ds033607.mlab.com:33607/heroku_fbnj3c1m';
 
 config.SimplifyPay = Simplify.getClient({
-    publicKey: 'sbpb_NWVkNzAxMDctYmI4MS00N2Y5LThkYzAtYTU5NjgzZjI3ZTA5',
-    privateKey: 'XzPJNTgI8l12CzmrpS9EFGf85MJtiDejlN2JwGfw45h5YFFQL0ODSXAOkNtXTToq'
+    publicKey: 'INSERT YOUR SIMPLIFY PUBLIC KEY',
+    privateKey: 'INSERT YOUR SIMPLIFY PRIVATE KEY'
 });
